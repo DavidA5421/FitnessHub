cb59141492a42c519e97c1b3a998ee49203e001a
David542 <david542agr@gmail.com>
2023-11-08 18:34:17 -0500

    Visual Redesign v1.0

---
+++ codes/nav_code.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded",function(){
+     //------------ Inicio ----------------------------------------
+
+    // script del menu responsive Abrir el menu
+    var btnMobile = document.getElementById('btn-mobile')
+    btnMobile.addEventListener('click', function (e) {
+        e.preventDefault()
+        let mySidenav = document.getElementById("mySidenav")
+        mySidenav.classList.toggle("openOffCanvas")
+    })
+
+    // script del menu responsive sticky menu
+
+    var nav = document.getElementById('mySidenav')
+
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > nav.offsetTop) {
+            nav.classList.add('nav-fixed')
+        } else {
+            nav.classList.remove('nav-fixed')
+        }
+    })
+    
+    // script del menu responsive effecto accordeon
+    var submenu = document.getElementsByClassName('link-submenu')
+
+    for (var i = 0; i < submenu.length; i++) {
+        submenu[i].onclick = function () {
+            var content = this.nextElementSibling
+
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px"
+            }
+
+        }
+    }
+});
